ff38854e50eb5f7210a014275ccb1f6104d0fcff
Maxim Cherepanov <m.cherepanov@dokltd.ru>
2020-11-09 16:50:36 +0300

    deleted debug print

--- merge_sort.py
+++ merge_sort.py
@@ -7,21 +7,13 @@ def Merge(A, p, q, r):
     #b_index = 0
     B = []
     p -= 1
-    #q -= 1
-    #r -= 1
     left = p
     right = r
     middle = q
     middle_point = middle
-    
-    #print(A)
-    #print(A[left:middle], A[middle:right])
-        
-    #print('--sort-->> p=' + str(left) + ' q=' + str(middle) + ' r=' + str(right) + ' B:' + str(B))
         
     while(left < middle_point and middle < right):
         if(A[left] <= A[middle]):
-            #print('LEFT: ' + str(A[left]) + ' <= ' + str(A[middle]))
             B.append(A[left])
             #B[b_index] = A[left]
             #b_index += 1
@@ -29,14 +21,10 @@ def Merge(A, p, q, r):
             
         else:
             B.append(A[middle])
-            #print('RIGHT ' + str(A[left]) + ' > ' + str(A[middle]))
             #B[b_index] = A[middle]
             #b_index += 1
             middle += 1
 
-    
-    #print('--sort-->> p=' + str(left) + ' q=' + str(middle) + ' r=' + str(right) + ' B:' + str(B))
-    
     L = []
     while(left < middle_point):
         L.append(A[left])
@@ -60,28 +48,16 @@ def Merge(A, p, q, r):
     for i in range(q, r):
         L2.append(A[i])
     
-    #print('L:' + str(L) + ' R:' + str(R) + ' = ' + str(B) + ' L1 = ' + str(L1) + ' L2 = ' + str(L2) +  '\n')
-    
     C = []
     for i in range(p, r):
         A[i] = B[i - p]
-        #print(i - p + 1)
-#        A[i] = B[i - p - 1]
-#        A[i] = B[i]
-#        C.append(B[i - p - 1])
-    #print('A: ' + str(A))
-    #print('C: ' + str(C))
-    #print('B: ' + str(B))
-    #print('\n')
 
 
 def Sort(A, p, r):
     if(p < r):
         q = (p + r)//2
-        #print('p=' + str(p) + ' q=' + str(q) + ' r=' + str(r))
         Sort(A, p, q)
         Sort(A, q+1, r)
-        #print("-------------")
         Merge(A, p, q, r)
 
 
@@ -91,7 +67,7 @@ def main():
     #A = [5,2,4,6,1,3,2,6]
     #A = [5,2,4,6,1,3]
     A = []
-    for i in range(1000):
+    for i in range(10000):
         A.append(randint(0, 99))
     #A = [1, 3, 5, 7, 2, 4, 6, 8]
     B = []
